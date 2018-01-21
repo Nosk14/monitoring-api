f107b28d68e652222223100a006863ca8b7c76c9
Carlos Lucas <nosk14@gmail.com>
2018-01-21 23:33:26 +0100

    store and retrieve methods
    Implemented store method
    Implemented retrieve method
    Implemented some methods to check data format

--- monitoring-api/api.py
+++ monitoring-api/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from datetime import datetime
 from dbmanager import DBManager
 
 app = Flask(__name__)
@@ -7,16 +8,31 @@ db = DBManager()
 
 @app.route('/data', methods=['GET', 'POST'])
 def data():
+    response = {'error': None}
     if request.method == 'GET':
         from_date = request.args.get('from')
         to_date = request.args.get('to')
         id = request.args.get('id')
-        # retrieve and return data
+
+        check_date(from_date)
+        check_date(to_date)
+        check_id(id)
+
+        results = db.retrieve(id, from_date, to_date)
+
+        response['id'] = id
+        response['data'] = results
 
     elif request.method == 'POST':
-        pass # store new data
+        json_data = request.get_json()
+        if 'id' not in json_data or 'data' not in json_data:
+            raise Exception('id and data must be specified.')
 
-    return ""
+        data = process_data(json_data['id'], json_data['data'])
+
+        db.store(data)
+
+    return jsonify(response)
 
 
 @app.route('/info', methods=['GET'])
@@ -30,3 +46,22 @@ def info():
 @app.errorhandler(500)
 def error_handler(ex):
     return jsonify({'error': str(ex)})
+
+
+def check_date(date):
+    datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+
+
+def check_id(id):
+    if not id:
+        raise Exception("id cannot be empty")
+
+
+def process_data(id, data):
+    check_id(id)
+    ret = []
+    for d in data:
+        check_date(d['time'])
+        ret.append([id, d.get('time'), d.get('temperature'), d.get('humidity')])
+
+    return ret
--- monitoring-api/dbmanager.py
+++ monitoring-api/dbmanager.py
@@ -6,6 +6,7 @@ class DBManager:
     def __init__(self):
         self.NAME = 'monitored_data.db'
         self.__db = sqlite3.connect(self.NAME)
+        self.__db.row_factory = sqlite3.Row
         self.__init_db()
 
     def __init_db(self):
@@ -21,11 +22,30 @@ class DBManager:
         finally:
             cursor.close()
 
-    def store(self, id, data):
-        pass
+    def store(self, data):
+        cursor = self.__db.cursor()
+        try:
+            cursor.executemany("""INSERT INTO 
+            data (id, time, temperature, humidity)
+            values (?,?,?,?)""", data)
+            self.__db.commit()
+        finally:
+            cursor.close()
 
     def retrieve(self, id, from_date, to_date):
-        pass
+        data = []
+        cursor = self.__db.cursor()
+        try:
+            cursor.execute("""SELECT time, temperature, humidity 
+            FROM data
+            WHERE id=? AND time>=? AND time<=?
+            ORDER BY time ASC""", (id, from_date, to_date))
+            rows = cursor.fetchall()
+            data = [dict(r) for r in rows]
+        finally:
+            cursor.close()
+
+        return data
 
     def list_ids(self):
         ids = []
