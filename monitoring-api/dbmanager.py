f373a0fe406599e1cc81ae6b5b738694294d8c74
Carlos Lucas <nosk14@gmail.com>
2018-05-27 22:49:43 +0200

    dbmanager refactor to use a new connection for every operation

--- monitoring-api/dbmanager.py
+++ monitoring-api/dbmanager.py
@@ -5,53 +5,55 @@ class DBManager:
 
     def __init__(self, path):
         self.path = path
-        self.__db = sqlite3.connect(self.path)
-        self.__db.row_factory = sqlite3.Row
         self.__init_db()
 
     def __init_db(self):
-        cursor = self.__db.cursor()
-        try:
-            cursor.execute("""CREATE TABLE IF NOT EXISTS data (
-                zone STRING NOT NULL,
-                time STRING NOT NULL,
-                temperature REAL,
-                humidity REAL,
-                PRIMARY KEY (zone, time)                 
-                )""")
-        finally:
-            cursor.close()
+        with sqlite3.connect(self.path) as db:
+            cursor = db.cursor()
+            try:
+                cursor.execute("""CREATE TABLE IF NOT EXISTS data (
+                    zone STRING NOT NULL,
+                    time STRING NOT NULL,
+                    temperature REAL,
+                    humidity REAL,
+                    PRIMARY KEY (zone, time)                 
+                    )""")
+            finally:
+                cursor.close()
 
     def store(self, data):
-        cursor = self.__db.cursor()
-        try:
-            cursor.executemany("""INSERT INTO 
-            data (zone, time, temperature, humidity)
-            values (?,?,?,?)""", data)
-            self.__db.commit()
-        finally:
-            cursor.close()
+        with sqlite3.connect(self.path) as db:
+            cursor = db.cursor()
+            try:
+                cursor.execute("""INSERT INTO 
+                data (zone, time, temperature, humidity)
+                values (?,?,?,?)""", data)
+                db.commit()
+            finally:
+                cursor.close()
 
     def retrieve(self, zone, from_date, to_date):
-        cursor = self.__db.cursor()
-        try:
-            cursor.execute("""SELECT time, temperature, humidity 
-            FROM data
-            WHERE id=? AND time>=? AND time<=?
-            ORDER BY time ASC""", (zone, from_date, to_date))
-            rows = cursor.fetchall()
-            data = [dict(r) for r in rows]
-        finally:
-            cursor.close()
-
+        with sqlite3.connect(self.path) as db:
+            db.row_factory = sqlite3.Row
+            cursor = db.cursor()
+            try:
+                cursor.execute("""SELECT time, temperature, humidity 
+                FROM data
+                WHERE zone=? AND time>=? AND time<=?
+                ORDER BY time ASC""", (zone, from_date, to_date))
+                rows = cursor.fetchall()
+                data = [dict(r) for r in rows]
+            finally:
+                cursor.close()
         return data
 
     def list_zones(self):
-        cursor = self.__db.cursor()
-        try:
-            cursor.execute('SELECT DISTINCT zone FROM data ORDER BY zone')
-            zones = [r[0] for r in cursor.fetchall()]
-        finally:
-            cursor.close()
-
+        with sqlite3.connect(self.path) as db:
+            db.row_factory = sqlite3.Row
+            cursor = db.cursor()
+            try:
+                cursor.execute('SELECT DISTINCT zone FROM data ORDER BY zone')
+                zones = [r[0] for r in cursor.fetchall()]
+            finally:
+                cursor.close()
         return zones
